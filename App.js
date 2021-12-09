059c228b6742d3267590a5533c4d2d27352dc041
eledu <develmdq@gmail.com>
2021-12-09 10:47:41 -0300

    proyect init

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text, ImageBackground} from 'react-native';
+
+const App = () => {
+  return <>
+    <Text>Hola</Text>
+  </>;
+};
+
+export default App;
